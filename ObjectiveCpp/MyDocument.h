6dedfa09e8837dc8728ee01ca982faf71d5fdeb3
Developer <>
2015-02-16 11:19:56 -0430

    put together all IBOutlets and IBActions in MyDocumen's header file

--- ObjectiveCpp/MyDocument.h
+++ ObjectiveCpp/MyDocument.h
@@ -18,11 +18,7 @@
 @interface MyDocument : NSDocument <AddItemWithStepsProtocol, OpenGLSceneViewDelegate, OpenGLTransforming>
 {
 @public // public for unit tests
-	BOOL manipulationFinished;
-	ItemCollection *items;
-	OpenGLManipulatingController *itemsController;
-	OpenGLManipulatingController *meshController;
-	id<OpenGLManipulating> manipulated;
+
 	IBOutlet OpenGLSceneView *viewLeft;
 	IBOutlet OpenGLSceneView *viewTop;
 	IBOutlet OpenGLSceneView *viewFront;
@@ -30,27 +26,50 @@
 	IBOutlet NSPopUpButton *editModePopUp;
 	IBOutlet NSPopUpButton *viewModePopUp;
 	IBOutlet AddItemWithStepsSheetController *addItemWithStepsSheetController;
+	IBOutlet NSSplitView *topSplit;
+	IBOutlet NSSplitView *bottomSplit;
+	IBOutlet NSSplitView *mainSplit;
+	IBOutlet NSTableView *propertyView;
+
+	BOOL manipulationFinished;
+	ItemCollection *items;
+	OpenGLManipulatingController *itemsController;
+	OpenGLManipulatingController *meshController;
+	id<OpenGLManipulating> manipulated;
+
 	enum MeshType itemWithSteps;
 	
 	NSMutableArray *views;
 	NSMutableArray *oldManipulations;
 	MeshManipulationState *oldMeshManipulation;
-	
-	IBOutlet NSSplitView *topSplit;
-	IBOutlet NSSplitView *bottomSplit;
-	IBOutlet NSSplitView *mainSplit;
-	
+
 	BOOL oneView;
 	
 	PropertyReflector *propertyReflector;
-	IBOutlet NSTableView *propertyView;
-	
+
 	ExperimentalBulletWrapper *bulletWrapper;
 	BOOL simulationRunning;
 	OpenGLManipulatingController *bulletController;
 }
 
 @property (readwrite, assign) id<OpenGLManipulating> manipulated;
+
+- (IBAction)addCube:(id)sender;
+- (IBAction)addCylinder:(id)sender;
+- (IBAction)addSphere:(id)sender;
+- (IBAction)changeEditMode:(id)sender;
+- (IBAction)changeManipulator:(id)sender;
+- (IBAction)changeViewMode:(id)sender;
+- (IBAction)mergeSelected:(id)sender;
+- (IBAction)splitSelected:(id)sender;
+- (IBAction)mergeVertexPairs:(id)sender;
+- (IBAction)cloneSelected:(id)sender;
+- (IBAction)flipSelected:(id)sender;
+- (IBAction)deleteSelected:(id)sender;
+- (IBAction)selectAll:(id)sender;
+- (IBAction)invertSelection:(id)sender;
+- (IBAction)play:(id)sender;
+- (IBAction)pause:(id)sender;
 
 - (Mesh *)currentMesh;
 - (MyDocument *)prepareUndoWithName:(NSString *)actionName;
@@ -65,31 +84,15 @@
 - (void)mergeSelectedItems;
 - (void)addItemWithType:(enum MeshType)type steps:(uint)steps;
 - (void)removeItemWithType:(enum MeshType)type steps:(uint)steps;
-- (IBAction)addCube:(id)sender;
-- (IBAction)addCylinder:(id)sender;
-- (IBAction)addSphere:(id)sender;
 - (void)editMeshWithMode:(enum MeshSelectionMode)mode;
 - (void)editItems;
-- (IBAction)changeEditMode:(id)sender;
-- (IBAction)changeManipulator:(id)sender;
-- (IBAction)changeViewMode:(id)sender;
-- (IBAction)mergeSelected:(id)sender;
-- (IBAction)splitSelected:(id)sender;
-- (IBAction)mergeVertexPairs:(id)sender;
-- (IBAction)cloneSelected:(id)sender;
 - (void)redoCloneSelected:(NSMutableArray *)selection;
 - (void)undoCloneSelected:(NSMutableArray *)selection;
-- (IBAction)flipSelected:(id)sender;
-- (IBAction)deleteSelected:(id)sender;
 - (void)redoDeleteSelected:(NSMutableArray *)selectedItems;
 - (void)undoDeleteSelected:(NSMutableArray *)selectedItems;
-- (IBAction)selectAll:(id)sender;
-- (IBAction)invertSelection:(id)sender;
 - (void)readFromTmd:(NSString *)fileName;
 - (BOOL)readFromModel3D:(NSString *)fileName;
 - (void)writeToModel3D:(NSString *)fileName;
 - (void)toggleOneViewFourView:(id)sender;
-- (IBAction)play:(id)sender;
-- (IBAction)pause:(id)sender;
 
 @end
